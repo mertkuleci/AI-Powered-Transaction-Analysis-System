ca1693ee2e02c4ead4c9951d76d4751ee289100e
mertkuleci <77067371+mertkuleci@users.noreply.github.com>
2025-01-31 16:54:57 +0300

    Update vite.config.js

--- ai-frontend/vite.config.js
+++ ai-frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // Allows external access
+    port: 3000, // Default port, change if needed
+  },
+  preview: {
+    allowedHosts: ['ai-powered-transaction-analysis-system-1.onrender.com'], // Add your Render domain here
+  },
+});
